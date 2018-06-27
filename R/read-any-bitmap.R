0e2ace2e03b248763644523082802a1601743241
Gregory Jefferis <jefferis@gmail.com>
2018-06-27 12:48:52 +0000

    version 0.1.5

--- R/read-any-bitmap.R
+++ R/read-any-bitmap.R
@@ -1,16 +1,16 @@
 # Functions to identify and read a variety of bitmap image formats.
 ###############################################################################
 
-#' Identify the type of an image using the magic value at the start of the file 
+#' Identify the type of an image using the magic value at the start of the file
 #'
-#' Currently works for png, jpeg and BMP images.
-#' Will seek to start of file if passed a connection.
-#' For details of magic values for files, see e.g. 
-#' http://en.wikipedia.org/wiki/Magic_number_(programming)#Magic_numbers_in_files
+#' Currently works for png, jpeg, BMP, and tiff images. Will seek to start of
+#' file if passed a connection. For details of magic values for files, see e.g.
+#' \url{http://en.wikipedia.org/wiki/Magic_number_(programming)#Magic_numbers_in_files}
 #' @param source Path to file or connection
-#' @param Verbose Whether to write a message to console on failure (Default F)
-#' @return character value corresponding to standard file extension of 
-#'   image format (i.e. jpg, png, bmp) or NA_character_ on failure.
+#' @param Verbose Whether to write a message to console on failure (Default
+#'   \code{FALSE})
+#' @return character value corresponding to standard file extension of image
+#'   format (i.e. jpg, png, bmp, tif) or \code{NA_character_} on failure.
 #' @export
 #' @examples
 #' jpegfile=system.file("img", "Rlogo.jpg", package="jpeg")
@@ -30,27 +30,35 @@ image_type<-function(source,Verbose=FALSE){
     return('png')
   else if(isTRUE(all.equal(magic[1:2], c(0xFF, 0xD8))))
     return('jpg')
-  # otherwise we failed to identify the file
+  else if(isTRUE(all.equal(magic[1:4], c(0x49, 0x49, 0x2A, 0x00))))
+    return('tif')
+  else if(isTRUE(all.equal(magic[1:4], c(0x4D, 0x4D, 0x00, 0x2A))))
+    return('tif')  # otherwise we failed to identify the file
   if(Verbose) warning("Failed to identify image type of: ",source,
         ' with magic: ',format.hexmode(as.raw(magic)))
   return(NA_character_)
 }
 
-#' Read in a bitmap image in JPEG, PNG or BMP format
+#' Read in a bitmap image in JPEG, PNG, BMP or TIFF format
 #'
-#' By default uses magic byte to identify file 
-#'   (rather than the file extension)
-#' Currently uses readers in bmp, jpeg and png packages.
+#' By default uses magic bytes at the start of the file to identify the image
+#' type (rather than the file extension). Currently uses readers in bmp, jpeg,
+#' png, and tiff packages.
 #' @param f Path to image file
 #' @param channel Integer identifying channel to return for an RGB image
 #' @param IdentifyByExtension Identify by file extension only (Default FALSE)
 #' @param ... Additional parameters passed to underlying image readers
-#' @return return value
+#' @return Objects returned by \code{\link[jpeg]{readJPEG}},
+#'   \code{\link[png]{readPNG}}, \code{\link[bmp]{read.bmp}}, or
+#'   \code{\link[tiff]{readTIFF}}. See their documentation for details.
 #' @importFrom png readPNG
 #' @importFrom jpeg readJPEG
 #' @importFrom bmp read.bmp
+#' @importFrom tiff readTIFF
 #' @export
-#' @seealso \code{\link[jpeg]{readJPEG},\link[png]{readPNG},\link[bmp]{read.bmp}}
+#' @seealso \code{\link{image_type}}, \code{\link[jpeg]{readJPEG}},
+#'   \code{\link[png]{readPNG}}, \code{\link[bmp]{read.bmp}},
+#'   \code{\link[tiff]{readTIFF}}
 #' @examples
 #' img1=read.bitmap(system.file("img", "Rlogo.jpg", package="jpeg"))
 #' str(img1)
@@ -67,9 +75,17 @@ read.bitmap<-function(f,channel,IdentifyByExtension=FALSE,...){
     ext=image_type(f)
   
   readfun=switch(ext,png=readPNG,jpeg=readJPEG,jpg=readJPEG,bmp=read.bmp,
-      stop("File f: ",f," does not appear to be a PNG, BMP or JPEG"))
+                 tif=readTIFF,tiff=readTIFF,
+                 stop("File f: ",f," does not appear to be a PNG, BMP, JPEG, or TIFF"))
   im=readfun(f,...)
   
   if(!missing(channel) && length(dim(im))==3) im=im[,,channel]
+  #BMP stores the alpha channel first (ARGB order), convert to RGBA for consistency with PNG files
+  if (ext=="bmp" && (length(dim(im))==3) && dim(im)[3] == 4)
+  {
+    att <- attributes(im)
+    im <- array(c(im[,,2:4],im[,,1]),dim(im))
+    attributes(im) <- att
+  }
   im
 }
--- README.md
+++ README.md
@@ -1,38 +1,47 @@
-Introduction
-============
+# readbitmap
 [![Build Status](https://travis-ci.org/jefferis/readbitmap.svg)](https://travis-ci.org/jefferis/readbitmap)
+[![CRAN status](http://www.r-pkg.org/badges/version/readbitmap)](https://cran.r-project.org/package=readbitmap)
 
-  * Simple R package to read bitmap images
-  * Currently supports BMP, JPEG and PNG images
-  * ... via [bmp](http://cran.r-project.org/web/packages/bmp/), [jpeg](http://cran.r-project.org/web/packages/jpeg/) and [png](http://cran.r-project.org/web/packages/png/) packages. 
+## Introduction
+
+**readbitmap** is a simple R package that provides a function `read.bitmap` to read
+standard bitmap format images, identifying them by their image header 
+(or [magic value](https://en.wikipedia.org/wiki/Magic_number_(programming)#Magic_numbers_in_files))
+rather than relying on the file extension.
+
+  * See http://jefferis.github.io/readbitmap for online documentation.
+  * Currently supports BMP, JPEG, PNG and TIFF images
+  * ... via [bmp](https://cran.r-project.org/package=bmp), [jpeg](https://cran.r-project.org/package=jpeg), [png](https://cran.r-project.org/package=png), and
+  [tiff](https://cran.r-project.org/package=tiff) packages. 
   * includes function to identify images by magic values 
     (standard file extensions can also be used)
   * Includes simple test suite
 
-Installation
-============
-Regular CRAN Install
---------------------
+## Installation
+
+### Regular CRAN Install
+
 ```r
 install.packages('readbitmap')
 ```
 
-Latest Version
---------------
+### Latest Version
+
 ```r
 if (!require("devtools")) install.packages("devtools")
-devtools::install_github('readbitmap','jefferis')
+devtools::install_github('jefferis/readbitmap')
 ```
 
-Dependencies
-------------
-The [jpeg](http://cran.r-project.org/web/packages/jpeg/) and 
-[png](http://cran.r-project.org/web/packages/png/) R packages depend on system
-libjpeg and libpng libraries.  These are pretty much always installed on unix
+## Dependencies
+The [jpeg](https://cran.r-project.org/package=jpeg),
+[png](https://cran.r-project.org/package=png), and 
+[tiff](https://cran.r-project.org/package=tiff) R packages depend on system
+libjpeg, libpng and libtiff libraries.  These are pretty much always installed on unix
 like systems (e.g. linux, macosx). Failing that, see:
 
   * http://www.libpng.org/
   * http://ijg.org/
+  * http://www.libtiff.org/
 
 For Windows, binary installs of the jpeg and png packages include the libjpeg
 and lipng available on winbuilder and therefore no system library is required.
@@ -40,7 +49,7 @@ The winbuilder libraries are part of a collection provied by Brian Ripley at:
 
   http://www.stats.ox.ac.uk/pub/Rtools/goodies/multilib/
 
-file local300.zip. 
+The file is called `local323.zip` at the time of writing.
 
 For windows users building from source, the libraries are part of the Rtools
 bundle available at:
